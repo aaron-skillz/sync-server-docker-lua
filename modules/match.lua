1c3c6161548d506aaa376150122d07663db1c373
Aaron Jones <ajones@skillz.com>
2020-08-11 20:20:38 -0700

    updated scripts with more examples and no more docker mounts needed

--- modules/match.lua
+++ modules/match.lua
@@ -43,13 +43,30 @@ function M.match_leave(context, dispatcher, tick, state, presences)
 end
 
 function M.match_loop(context, dispatcher, tick, state, messages)
+  -- Messages format:
+  -- {
+  --   {
+  --     sender = {
+  --       user_id = "user unique ID",
+  --       session_id = "session ID of the user's current connection",
+  --       username = "user's unique username",
+  --       node = "name of the Nakama node the user is connected to"
+  --     },
+  --     op_code = 1, -- numeric op code set by the sender.
+  --     data = "any string data set by the sender" -- may be nil.
+  --   },
+  --   ...
+  -- }
+
+
   -- list of clients to send data to that doesn't include the sender
   local msgTargets = {}
   -- for _, presence in pairs(state.presences) do
   --   nk.logger_debug(string.format("Presence %s named %s", presence.user_id, presence.username))
   -- end
   for _, message in ipairs(messages) do
-    nk.logger_debug(string.format("Received %s from %s", message.data, message.sender.username))
+    nk.logger_debug(string.format("Received %s from %s (%s) (opcode=%s)", message.data, message.sender.username, 
+      message.sender.user_id, message.op_code))
     local decoded = nk.json_decode(message.data)
     -- for k, v in pairs(decoded) do
     --   nk.logger_debug(string.format("Message key %s contains value %s", k, v))
@@ -64,7 +81,7 @@ function M.match_loop(context, dispatcher, tick, state, messages)
       end
     end
     if table.getn(msgTargets) > 0 then
-      dispatcher.broadcast_message(1, message.data, msgTargets)
+      dispatcher.broadcast_message(2, message.data, msgTargets)
     end
   end
   return state
